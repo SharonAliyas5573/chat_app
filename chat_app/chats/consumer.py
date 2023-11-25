ea5b38c681a444ade277415b39f96c26ddf34cd7
sharon <sharonaliyas25@gmail.com>
2023-11-25 07:56:23 +0530

    Fixed error in chats

--- chat_app/chats/consumer.py
+++ chat_app/chats/consumer.py
@@ -1,11 +1,16 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import JsonWebsocketConsumer
-from .models import Conversation
-from .api.serializers import MessageSerializer
 import json
 from uuid import UUID
-from .models import Message
-from chat_app.users.models import User
+
+from asgiref.sync import async_to_sync
+from django.contrib.auth import get_user_model
+from channels.generic.websocket import JsonWebsocketConsumer
+from conversa_dj.chats.api.serializers import MessageSerializer
+
+from conversa_dj.chats.models import Conversation, Message
+
+
+User = get_user_model()
+
 
 class UUIDEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -31,26 +36,58 @@ class ChatConsumer(JsonWebsocketConsumer):
         self.user = self.scope["user"]
         if not self.user.is_authenticated:
             return
+
         self.accept()
-        print("connected!")
-        try:
-            self.conversation_name = f"{self.scope['url_route']['kwargs']['conversation_name']}"
-        except Exception as e:
-            print(e)
-        self.conversation, created = Conversation.objects.get_or_create(name=self.conversation_name)
+        self.conversation_name = (
+            f"{self.scope['url_route']['kwargs']['conversation_name']}"
+        )
+        self.conversation, created = Conversation.objects.get_or_create(
+            name=self.conversation_name
+        )
 
         async_to_sync(self.channel_layer.group_add)(
             self.conversation_name,
             self.channel_name,
         )
-        messages = self.conversation.messages.all().order_by("-timestamp")[0:50]
-        self.send_json({
+
+        self.send_json(
+            {
+                "type": "online_user_list",
+                "users": [user.username for user in self.conversation.online.all()],
+            }
+        )
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.conversation_name,
+            {
+                "type": "user_join",
+                "user": self.user.username,
+            },
+        )
+
+        self.conversation.online.add(self.user)
+
+        messages = self.conversation.messages.all().order_by("-timestamp")[0:10]
+        message_count = self.conversation.messages.all().count()
+        self.send_json(
+            {
                 "type": "last_50_messages",
                 "messages": MessageSerializer(messages, many=True).data,
-            })
+                "has_more": message_count > 5,
+            }
+        )
 
     def disconnect(self, code):
-        print("Disconnected!")
+        if self.user.is_authenticated:
+            # send the leave event to the room
+            async_to_sync(self.channel_layer.group_send)(
+                self.conversation_name,
+                {
+                    "type": "user_leave",
+                    "user": self.user.username,
+                },
+            )
+            self.conversation.online.remove(self.user)
         return super().disconnect(code)
 
     def get_receiver(self):
@@ -62,13 +99,40 @@ class ChatConsumer(JsonWebsocketConsumer):
 
     def receive_json(self, content, **kwargs):
         message_type = content["type"]
+
+        if message_type == "read_messages":
+            messages_to_me = self.conversation.messages.filter(to_user=self.user)
+            messages_to_me.update(read=True)
+
+            # Update the unread message count
+            unread_count = Message.objects.filter(to_user=self.user, read=False).count()
+            async_to_sync(self.channel_layer.group_send)(
+                self.user.username + "__notifications",
+                {
+                    "type": "unread_count",
+                    "unread_count": unread_count,
+                },
+            )
+
+        if message_type == "typing":
+            async_to_sync(self.channel_layer.group_send)(
+                self.conversation_name,
+                {
+                    "type": "typing",
+                    "user": self.user.username,
+                    "typing": content["typing"],
+                },
+            )
+
         if message_type == "chat_message":
+
             message = Message.objects.create(
                 from_user=self.user,
                 to_user=self.get_receiver(),
                 content=content["message"],
                 conversation=self.conversation,
             )
+
             async_to_sync(self.channel_layer.group_send)(
                 self.conversation_name,
                 {
@@ -77,12 +141,80 @@ class ChatConsumer(JsonWebsocketConsumer):
                     "message": MessageSerializer(message).data,
                 },
             )
+
+            notification_group_name = self.get_receiver().username + "__notifications"
+            async_to_sync(self.channel_layer.group_send)(
+                notification_group_name,
+                {
+                    "type": "new_message_notification",
+                    "name": self.user.username,
+                    "message": MessageSerializer(message).data,
+                },
+            )
+
         return super().receive_json(content, **kwargs)
 
     def chat_message_echo(self, event):
-        
+        self.send_json(event)
+
+    def user_join(self, event):
+        self.send_json(event)
+
+    def user_leave(self, event):
+        self.send_json(event)
+
+    def typing(self, event):
+        self.send_json(event)
+
+    def new_message_notification(self, event):
+        self.send_json(event)
+
+    def unread_count(self, event):
         self.send_json(event)
 
     @classmethod
     def encode_json(cls, content):
         return json.dumps(content, cls=UUIDEncoder)
+
+
+class NotificationConsumer(JsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.user = None
+        self.notification_group_name = None
+
+    def connect(self):
+        self.user = self.scope["user"]
+        if not self.user.is_authenticated:
+            return
+
+        self.accept()
+
+        # private notification group
+        self.notification_group_name = self.user.username + "__notifications"
+        async_to_sync(self.channel_layer.group_add)(
+            self.notification_group_name,
+            self.channel_name,
+        )
+
+        # Send count of unread messages
+        unread_count = Message.objects.filter(to_user=self.user, read=False).count()
+        self.send_json(
+            {
+                "type": "unread_count",
+                "unread_count": unread_count,
+            }
+        )
+
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.notification_group_name,
+            self.channel_name,
+        )
+        return super().disconnect(code)
+
+    def new_message_notification(self, event):
+        self.send_json(event)
+
+    def unread_count(self, event):
+        self.send_json(event)
